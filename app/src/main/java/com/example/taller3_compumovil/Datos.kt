70de986de40af3e77a7189cc71d9963fc1b4063e
Adrian Ruiz <adrianrrruiz@gmail.com>
2024-05-01 16:59:43 -0500

    Real time database on register ready

---
+++ app/src/main/java/com/example/taller3_compumovil/Datos.kt
@@ -0,0 +1,7 @@
+package com.example.taller3_compumovil
+
+class Datos {
+    companion object{
+        const val MY_PERMISSION_REQUEST_LOCATION = 23
+    }
+}
--- app/src/main/java/com/example/taller3_compumovil/RegisterActivity.kt
+++ app/src/main/java/com/example/taller3_compumovil/RegisterActivity.kt
@@ -1,25 +1,55 @@
 package com.example.taller3_compumovil
 
 import android.content.Intent
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.example.taller3_compumovil.Datos.Companion.MY_PERMISSION_REQUEST_LOCATION
 import com.example.taller3_compumovil.databinding.ActivityRegisterBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.auth.auth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    //Auth
     private lateinit var auth: FirebaseAuth
+    //Realtime Database
+    private val database = FirebaseDatabase.getInstance()
+    private lateinit var myRef: DatabaseReference
+    val PATH_USERS="users/"
+    //Location permissions
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var myLocation: Location? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Inicializar el cliente de ubicación fusionada
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // Revisar permisos
+        checkLocationPermission()
 
         auth = Firebase.auth
 
@@ -33,10 +63,20 @@ class RegisterActivity : AppCompatActivity() {
                         val user = auth.currentUser
                         if (user != null) {
                             // Update user info
-                            val upcrb = UserProfileChangeRequest.Builder()
-                            upcrb.setDisplayName(binding.editTextName.text.toString() + " " + binding.editTextLastName.text.toString())
+                            //val upcrb = UserProfileChangeRequest.Builder()
+                            //upcrb.setDisplayName(binding.editTextName.text.toString() + " " + binding.editTextLastName.text.toString())
                             //upcrb.setPhotoUri(Uri.parse("path/to/pic")) //fake uri, use Firebase Storage
-                            user.updateProfile(upcrb.build())
+                            //user.updateProfile(upcrb.build())
+                            var usuario = Usuario()
+                            usuario.nombre = binding.editTextName.text.toString()
+                            usuario.apellido = binding.editTextLastName.text.toString()
+                            usuario.noIdentificacion = binding.editTextId.text.toString().toInt()
+                            if(myLocation != null){
+                                usuario.latitud = myLocation!!.latitude
+                                usuario.longitud = myLocation!!.longitude
+                            }
+                            myRef = database.getReference(PATH_USERS+user.uid)
+                            myRef.setValue(usuario)
                             updateUI(user)
                         }
                     } else {
@@ -48,6 +88,73 @@ class RegisterActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == MY_PERMISSION_REQUEST_LOCATION){// Nuestros permisos
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){ // Validar que si se aceptaron ambos permisos
+                // Permisos aceptados
+                startLocationUpdates()
+            }else{
+                //El permiso no ha sido aceptado
+                Toast.makeText(this, "Permisos denegados :(", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Si no tienes permisos, solicitarlos al usuario
+            requestLocationPermission()
+        } else {
+            // Si tienes permisos
+            // Get the last known location
+            startLocationUpdates()
+
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        // Crear una solicitud de ubicación
+        val locationRequest: LocationRequest = LocationRequest.create()
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        locationRequest.setInterval(10000) // Intervalo de actualización de ubicación en milisegundos
+
+        // Configurar un callback para recibir actualizaciones de ubicación
+        val locationCallback: LocationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                // Obtener la ubicación actual del resultado
+                val miUbi: Location? = locationResult.lastLocation
+                if (miUbi != null) {
+                    // Actualizar la interfaz de usuario con la ubicación actual
+                    myLocation = miUbi
+                }
+            }
+        }
+
+        // Solicitar actualizaciones de ubicación
+        fusedLocationClient!!.requestLocationUpdates(locationRequest, locationCallback, null)
+
+    }
+
+    private fun requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)){
+            //El usuario ya ha rechazado los permisos
+            Toast.makeText(this, "Permisos denegados :(", Toast.LENGTH_SHORT).show()
+        }else{
+            //Pedir permisos
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSION_REQUEST_LOCATION)
+        }
     }
 
     private fun updateUI(user: FirebaseUser) {
